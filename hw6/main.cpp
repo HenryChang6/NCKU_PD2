16a6dac59a3a8135db52ad0552d9fe661ba5780e
HenryChang6 <henry930619@gmail.com>
2023-05-28 21:59:27 +0800

    0528 NEED DEBUG

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -2,7 +2,8 @@
 #include <fstream> //檔案讀寫
 #include <sstream> //字串處理
 #include <map> 
-#include <vector>
+#include <queue>
+#include <utility> //for pair<>
 #include <algorithm> // For remove_if function //For transform function
 #include <cctype>    // For ispunct function //For tolower function
 #include <set>  
@@ -16,22 +17,27 @@ string process_word(const string& word);
 // Function to build the index from the corpus and count the idf_values of each words manually
 void build_index(map< string, set<int> >& index, map<string, double>& idf_values ,ifstream& corpus_file);
 // Function to process the queries and print the results
-void search_queries(map< string, set<int> >& index, map<string, double>& idf_values, ifstream& query_file);
-// Function to calculate the answer
-set<int> calculate_intersection(const vector< set<int> >& results);
+void search_queries(map< string, set<int> >& index, map<string, double>& idf_values, ifstream& query_file, int output_num);
+
+struct compare{
+    bool operator ()(const pair<int,double>& p1, const pair<int,double>& p2) {
+        if(p1.second != p2.second){
+            return (p1.second < p2.second);
+        }
+        return (p1.first > p2.first);
+    };
+};
 
 int main(int argc, char *argv[]) {
 	map< string, set<int> > index;
     map<string, double> idf_values;
-
-    assert(argc == 4);
 
    	ifstream corpus_file(argv[1]);
     build_index(index, idf_values, corpus_file);
    	corpus_file.close();
 
     ifstream query_file(argv[2]);
-    search_queries(index, idf_values, query_file);
+    search_queries(index, idf_values, query_file, stoi(argv[3]));
     query_file.close();
 
     return 0;
@@ -49,72 +55,93 @@ void process_word(string& word){
 
 void build_index(map< string, set<int> >& index, map<string, double>& idf_values ,ifstream& corpus_file){
     string line;
-    int total_docs = 0;
+    int total_corpus = 0;
     while(getline(corpus_file,line)){
         stringstream ss(line);
         string id_str , text ,temp;
         // Extract the document ID and the text from the line
-        getline(ss, id_str, ',');
-        //ws manipulator (which stands for "whitespace") is used here to skip any leading whitespace after the comma and before the starting double quote. 
+        getline(ss, id_str, ','); 
         getline(ss, temp,'"');
         int id = stoi(id_str);
-        ++total_docs;
+        ++total_corpus;
 
         // Tokenize the text into words
         string word;
-        set<string> words_in_docs;
+        set<string> words_in_corpus;
         while(getline(ss,word,' ')){
             process_word(word);
             if (!word.empty()){
                 index[word].insert(id);
-                words_in_docs.insert(word);
+                words_in_corpus.insert(word);
             } 
         }
-
         // Update the number of documents each word appears in
-        for(auto& word : words_in_docs){
+        for(auto& word : words_in_corpus){
             idf_values[word]++;
         }
     }
-
+    //count the idf_value
     for(auto& idf : idf_values){
-        idf.second = log(total_docs/idf.second);
+        idf.second = log(total_corpus/idf.second);
+        // cout<<idf.first<<" "<<idf.second<<endl;
     }
 }
 
-void search_queries(map< string, set<int> >& index, map<string, double>& idf_values, ifstream& query_file){
+void search_queries(map< string, set<int> >& index, map<string, double>& idf_values, ifstream& query_file, int output_num){
     string line;
     while(getline(query_file,line)){
         stringstream ss(line);
         string word;
-
-        set<string> query_words; // use a set to keep unique query words
-
-        // Tokenize the line into words
+        map<int, double> idf_sum;
+        // Tokenize the queries
         while(ss>>word){
             process_word(word);
-            if(!word.empty()) query_words.insert(word);
+            // Add this query's idf_value into each corpuses which contains this query 
+            if(idf_values.count(word)){
+                double idf = idf_values[word];
+                for(auto& id : index[word]) idf_sum[id]+=idf;
+            }
         }
-    }       
-}
+        //start sorting and output it
+
+        priority_queue<pair<int, double>, vector< pair<int, double> >, compare> pq;
+        int count_zero_idf = 0;
+        for(const auto& item : idf_sum){
+            pq.push(item);
+            if(item.second == 0) count_zero_idf++; 
+        }
+        //算出共有幾個IDF值非零的id 
+        int none_zero_num = pq.size() - count_zero_idf;
+        //特例： 若所有的idf都是0
+        if(pq.size() == count_zero_idf){
+            for(int i = 0; i < output_num;++i){
+                if(i == (output_num - 1)) cout<<"-1"<<endl;
+                else cout<<"-1"<<" ";
+            }
+            continue;
+        }
+        //特例：如果idf不為0的元素小於要印出來個數，其他用-1補上
+        if(none_zero_num < output_num){
+            int fill_negative_one = output_num - none_zero_num;
+            
+            for(int i = 0; i < none_zero_num; ++i){
+                auto pq_item = pq.top();
+                cout<<pq_item.first<<" ";
+                pq.pop();
+            }
+            for(int i = 0; i < fill_negative_one; ++i){
+                if(i == fill_negative_one-1) cout<<"-1"<<endl;
+                else cout<<"-1"<<" ";
+            }
+            continue;
+        }
 
 
-set<int> calculate_intersection(const vector< set<int> >& results){
-    /*
-    The set_intersection function is a built-in STL function that computes the intersection
-    of two sorted ranges. It requires five arguments: the beginning and end iterators of the
-    first range, the beginning and end iterators of the second range, and an output iterator
-    where the results are written.
-    */
-   set<int>intersection = results[0];
-   for(int i = 1; i < results.size(); i++){
-        set<int> temp;
-        set_intersection(intersection.begin(),intersection.end(),results[i].begin(),results[i].end(),inserter(temp,temp.begin()));
-        /*inserter(temp, temp.begin()): This is the output iterator where the results of the intersection are written. It's using
-        the inserter function to create an insert iterator that inserts new elements at the beginning of the temp set.*/
-        intersection.clear();
-        intersection.insert(temp.begin(),temp.end());
-        /*The move function is used here to transfer ownership of the temp set to the intersection set, rather than copying the elements.*/
-   }
-    return intersection;
+        for(int i = 0; i < output_num && !pq.empty(); ++i){
+            auto pq_item = pq.top();
+            if(i != (output_num - 1)) cout<<pq_item.first<<" ";
+            else cout<<pq_item.first<<endl;
+            pq.pop();
+        }
+    }
 }
--- hw6/notes.md
+++ hw6/notes.md
@@ -1,4 +1,9 @@
 # PD2 hw6 notes
 
 ## IDF(Inverse document frequency)
-The IDF of a term 't' is calculated as log(total number of documents / number of documents containing term 't').
+The IDF of a term 'HW6' is calculated as log(total number of the corpus / number of corpus containing term 'HW6').
+
+## Impoortant Output information
+    Principal:先照著idf_sum，idf_sum大印到小
+    If the top-k result contains strings with the same idf_sum, please sort them according to their S_ID in the ascending order. 
+    注意！！！ 總共只會有k筆輸出，不會變多變少。
