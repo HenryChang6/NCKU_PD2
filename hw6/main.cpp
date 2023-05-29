391a6376a3a00384bfbe5d97dd97ab648960f936
HenryChang6 <henry930619@gmail.com>
2023-05-30 04:28:52 +0800

    0530 DEBUG

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -1,3 +1,51 @@
+#pragma GCC optimize(3)
+#pragma GCC target("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-fwhole-program")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-skip-blocks")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("-funsafe-loop-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+
 #include <iostream>
 #include <fstream> //檔案讀寫
 #include <sstream> //字串處理
@@ -7,7 +55,7 @@
 #include <algorithm> // For remove_if function //For transform function
 #include <cctype>    // For ispunct function //For tolower function
 #include <set>  
-#include <math.h> //For log()
+#include <cmath> 
 #include <cassert>
 
 using namespace std;
@@ -29,6 +77,10 @@ struct compare{
 };
 
 int main(int argc, char *argv[]) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
 	map< string, set<int> > index;
     map<string, double> idf_values;
 
@@ -82,7 +134,7 @@ void build_index(map< string, set<int> >& index, map<string, double>& idf_values
     }
     //count the idf_value
     for(auto& idf : idf_values){
-        idf.second = log(total_corpus/idf.second);
+        idf.second = log10((double)total_corpus/idf.second);
         // cout<<idf.first<<" "<<idf.second<<endl;
     }
 }
